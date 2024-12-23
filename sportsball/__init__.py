3b848290bd0e613fbdcfe705f2b5b70dbe2a14c8
Will Sackfield <will.sackfield@gmail.com>
2024-12-23 23:13:05 +0000

    Add __VERSION__ to library

--- sportsball/__init__.py
+++ sportsball/__init__.py
@@ -1,6 +1,8 @@
 """The main module for sportsball."""
 # ruff: noqa: F401
 
+__VERSION__ = "0.0.2"
+
 from .data import (AddressModel, BookieModel, GameModel, League, LeagueModel,
                    PlayerModel, SeasonModel, SeasonType, TeamModel, VenueModel)
 from .portfolio import Portfolio
