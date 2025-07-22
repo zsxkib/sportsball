7304123e2df5ef06f3d6cd11355cff9e3da32b60
Will Sackfield <will.sackfield@gmail.com>
2025-07-22 16:21:53 -0400

    Add more locations

--- setup.py
+++ setup.py
@@ -26,7 +26,7 @@ def install_requires() -> typing.List[str]:
 
 setup(
     name='sportsball',
-    version='0.11.24',
+    version='0.11.25',
     description='A library for pulling in and normalising sports stats.',
     long_description=long_description,
     long_description_content_type='text/markdown',
